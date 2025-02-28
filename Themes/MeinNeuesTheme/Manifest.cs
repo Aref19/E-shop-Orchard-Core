f8dd4e9531e55c6ba86401902acc03b26fb18458
unknown <arefobaid6@gmail.com>
2025-03-01 00:29:00 +0100

    add Theme

---
+++ Themes/MeinNeuesTheme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "MeinNeuesTheme",
+    Author = "Aref Obaid",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "MeinNeuesTheme"
+)]
